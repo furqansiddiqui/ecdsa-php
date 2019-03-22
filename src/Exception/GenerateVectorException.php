115447028a12478784e9d9517d1381cb6958aa2f
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-03-22 15:05:54 +0500

    ECDSA Exceptions

---
+++ src/Exception/GenerateVectorException.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * This file is a part of "furqansiddiqui/ecdsa-php" package.
+ * https://github.com/furqansiddiqui/ecdsa-php
+ *
+ * Copyright (c) 2019 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/furqansiddiqui/ecdsa-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\ECDSA\Exception;
+
+/**
+ * Class GenerateVectorException
+ * @package FurqanSiddiqui\ECDSA\Exception
+ */
+class GenerateVectorException extends ECDSA_Exception
+{
+}
