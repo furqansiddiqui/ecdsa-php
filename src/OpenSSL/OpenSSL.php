451e3c1ea7a42f7bce47eda7bf6e62e6b3fc888d
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2019-03-22 15:33:33 +0500

    OpenSSL helpers

---
+++ src/OpenSSL/OpenSSL.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * This file is a part of "furqansiddiqui/ecdsa-php" package.
+ * https://github.com/furqansiddiqui/ecdsa-php
+ *
+ * Copyright (c) 2019 Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/furqansiddiqui/ecdsa-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\ECDSA\OpenSSL;
+
+use FurqanSiddiqui\DataTypes\Binary;
+
+/**
+ * Class OpenSSL
+ * @package FurqanSiddiqui\ECDSA\OpenSSL
+ */
+class OpenSSL
+{
+    /**
+     * @param string $data
+     * @param string $type
+     * @param string $eol
+     * @return string
+     */
+    public static function der2pem(string $data, string $type = "PRIVATE KEY", string $eol = "\n"): string
+    {
+        $type = strtoupper($type);
+        $pem = sprintf("-----BEGIN %s-----", $type) . $eol;
+        $pem .= chunk_split(base64_encode($data), 64, $eol);
+        $pem .= sprintf("-----END %s-----", $type) . $eol;
+
+        return $pem;
+    }
+
+    /**
+     * @param Binary $privateKey
+     * @return array
+     */
+    public static function Secp256k1(Binary $privateKey): array
+    {
+        $key = sprintf("302e0201010420%sa00706052b8104000a", $privateKey->get()->base16(false));
+        $pem = self::der2pem(hex2bin($key), "EC PRIVATE KEY");
+        return openssl_pkey_get_details(openssl_pkey_get_private($pem));
+    }
+}
