b6156818563e83bfef97bf05493ca61bcdde3eae
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-02-01 02:35:15 +0400

    exceptions

---
+++ src/Exception/ECDSA_RPC_Exception.php
@@ -0,0 +1,36 @@
+<?php
+/*
+ * This file is a part of "furqansiddiqui/ecdsa-php" package.
+ * https://github.com/furqansiddiqui/ecdsa-php
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/furqansiddiqui/ecdsa-php/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace FurqanSiddiqui\ECDSA\Exception;
+
+/**
+ * Class ECDSA_RPC_Exception
+ * @package FurqanSiddiqui\ECDSA\Exception
+ */
+class ECDSA_RPC_Exception extends \Exception
+{
+    /** @var \CurlHandle|null */
+    public ?\CurlHandle $curlHandle = null;
+
+    /**
+     * @param \CurlHandle $ch
+     * @return static
+     */
+    public static function CurlError(\CurlHandle $ch): static
+    {
+        $ex = new static();
+        $ex->curlHandle = $ch;
+        return $ex;
+    }
+}
